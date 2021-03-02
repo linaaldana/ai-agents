a3008b682ba5347671dd94f7bff8f833c06d4751
lina aldana <linaaldanavelez@gmail.com>
2021-03-02 09:03:36 -0500

    se realiza modificacion para guardar la cantidad de veces que el raton pasa por una posicion

--- src/ratonlab_entorno/ratonAgent.js
+++ src/ratonlab_entorno/ratonAgent.js
@@ -4,121 +4,76 @@ const Agent = require('../core/Agent');
  * Simple reflex agent. Search for an object whithin a labyrinth. 
  * If the object is found the agen take it.
  */
-class CleanerAgent extends Agent {
+class RatonAgent2 extends Agent {
     constructor(value) {
         super(value);
         //LEFT, UP, RIGHT, DOWN, CELL
         this.table = {
             "0,0,0,0,0": "UP",
             "0,0,0,1,0": "UP",
-
-            //
             "0,0,1,0,0": "UP",
-            "0,0,1,0,0,0": "DOWN",
-            "0,0,1,0,0,1": "DOWN",
-            //
-
-            // ---
             "0,0,1,1,0": "LEFT",
-            "0,0,1,1,0,0": "UP",
-            "0,0,1,1,0,1": "UP", //RIGHT / UP
-            // ---
-
-            "0,1,0,0,0": "RIGHT",
-            "0,1,0,0,0,0": "DOWN",
-            "0,1,0,0,0,1": "DOWN",
-
-            // ---
+            "0,1,0,0,0": "LEFT",
             "0,1,0,1,0": "RIGHT",
-            "0,1,0,1,0,0": "LEFT",
-            "0,1,0,1,0,1": "LEFT", ////RIGHT / LEFT 
-            // ---
-
             "0,1,1,0,0": "LEFT",
-
-            //
             "0,1,1,1,0": "LEFT",
-            "0,1,1,1,0,0": "LEFT",
-            "0,1,1,1,0,1": "LEFT",
-            //
-
-            //
             "1,0,0,0,0": "UP",
-            "1,0,0,0,0,0": "RIGHT",
-            "1,0,0,0,0,1": "RIGHT",
-            //
-
-            // ---
             "1,0,0,1,0": "RIGHT",
-            "1,0,0,1,0,0": "UP",
-            "1,0,0,1,0,1": "UP", // RIGHT / UP
-            // ---
-
-            // ---
             "1,0,1,0,0": "DOWN",
-            "1,0,1,0,0,0": "UP",
-            "1,0,1,0,0,1": "UP", // DOWN / UP
-            // ---
-
             "1,0,1,1,0": "UP",
-
-            // ---
             "1,1,0,0,0": "RIGHT",
-            "1,1,0,0,0,0": "DOWN",
-            "1,1,0,0,0,1": "DOWN", // RIGHT / DOWN
-            // ---
-
             "1,1,0,1,0": "RIGHT",
             "1,1,1,0,0": "DOWN",
             "default": "TAKE"
         };
-        this.bigNumber = 20;
-        this.memory = []
-        this.currentPosition = {}
+
+        this.bigNumber=20;
+        this.model = [];
+        this.currentPosition={};
+        this.actions = ["LEFT", "UP", "RIGHT", "DOWN"]
+
     }
 
-    /**
-     * 
-     * @param {*} initialValue 
-     * This function update de agent environment by de initial value received
-     */
-    setup(initialValue) {
-        for (let i = 0; i < this.bigNumber; i++) {
-            this.memory.push([])
-            for (let j = 0; j < this.bigNumber; j++) {
-                this.memory[i][j] = 0
+
+    setup(state0){
+        for(let i=0;i<this.bigNumber;i++){
+            this.model.push([]);
+            for(let j=0; j<this.bigNumber;j++){
+                this.model[i][j]=0;
             }
         }
-        this.memory[initialValue.y][initialValue.x] = this
-        this.currentPosition = { ...initialValue }
+        this.currentPosition = {...state0};
     }
 
-    /**
+     /**
      * 
-     * @param {*} perception 
-     * This function update agent memory with the perception reveived
+     * @param {*} number 
+     * This function gets the available position based on array ordered [LEFT, UP, RIGHT, DOWN] with the code received
      */
-    updateMemory(perception) {
+    getAvailablePosition(number) {
+        return this.actions[number]
+    }
+
+    updateModel(){
+        
         let left = 0, right = 0, up = 0, down = 0
         if (this.currentPosition.y > 0) {
-            up = this.currentPosition.y - 1
-            if (this.memory[up][this.currentPosition.x] === 0) this.memory[up][this.currentPosition.x] = perception[1]
+            up = this.currentPosition.y - 1            
+            if (this.model[up][this.currentPosition.x] === 0) this.model[up][this.currentPosition.x] = this.perception[1] * this.bigNumber
         }
         down = this.currentPosition.y + 1
-        if (this.memory[down][this.currentPosition.x] === 0) this.memory[down][this.currentPosition.x] = perception[3]
+        if (this.model[down][this.currentPosition.x] === 0) this.model[down][this.currentPosition.x] = this.perception[3] * this.bigNumber
         if (this.currentPosition.x > 0) {
             left = this.currentPosition.x - 1
-            if (this.memory[this.currentPosition.y][left] === 0) this.memory[this.currentPosition.y][left] = perception[0]
+            if (this.model[this.currentPosition.y][left] === 0) this.model[this.currentPosition.y][left] = this.perception[0] * this.bigNumber
         }
         right = this.currentPosition.x + 1
-        if (this.memory[this.currentPosition.y][right] === 0) this.memory[this.currentPosition.y][right] = perception[2]
+        if (this.model[this.currentPosition.y][right] === 0) this.model[this.currentPosition.y][right] = this.perception[2] * this.bigNumber
+
+        
     }
 
-    /**
-     * 
-     * @param {*} action 
-     * This function get the netx position in x, y coords on the board for the agent whit the action received
-     */
+    // this function returns the next position in the model depending on the selected action
     getNextPosition(action) {
         let x = this.currentPosition.x, y = this.currentPosition.y
         switch (action) {
@@ -140,27 +95,45 @@ class CleanerAgent extends Agent {
         return { x, y }
     }
 
-    /**
+     /**
      * 
      * @param {*} action 
-     * This function update the agent position on board memory and set de last position with 1
+     * This function updates the current agent's position and increments the value of the current position.
      */
     updateCurrentPosition(action) {
-        this.memory[this.currentPosition.y][this.currentPosition.x] = 1
-        this.currentPosition = { ...this.getNextPosition(action) }
-        this.memory[this.currentPosition.y][this.currentPosition.x] = this
-        return action
+        this.model[this.currentPosition.y][this.currentPosition.x] += 1
+        this.currentPosition = { ...this.getNextPosition(action) }       
     }
 
-    /**
-     * 
-     * @param {*} viewKey 
-     * THis function verify if the netx position is a postion not explored by the agent
-     */
-    verifyMovement(viewKey) {
-        let nextPosition = this.getNextPosition(this.table[viewKey])
-        if (this.memory[nextPosition.y][nextPosition.x] === 1) viewKey += ',0'
-        return viewKey
+    verifyMovement() {
+        let action = null;
+        let availablePositions=[], nextPositions=[];
+
+        if(this.perception[4]==1){            
+            action = "TAKE";
+            return action;
+        }     
+        
+        for(let i =0; i < this.perception.length-1;i++){
+            if (this.perception[i] === 0) {availablePositions.push(this.getAvailablePosition(i));} 
+        }
+        for(let i=0; i< availablePositions.length; i++){
+            let nextPosition=this.getNextPosition(availablePositions[i]);
+            nextPositions.push({ action: availablePositions[i], position: nextPosition, timesVisited: this.model[nextPosition.y][nextPosition.x] })
+        }
+        
+        let temp=this.bigNumber;
+        let index=0;
+        for(let i=0; i< nextPositions.length; i++){
+            if(nextPositions[i].timesVisited<temp){
+                temp=nextPositions[i].timesVisited;
+                index=i;
+            }
+            
+        }
+        
+        return nextPositions[index].action;      
+        
     }
 
     /**
@@ -168,16 +141,26 @@ class CleanerAgent extends Agent {
      * In this case, the state is just obtained as the join of the perceptions
      */
     send() {
-        this.updateMemory(this.perception)
-        let viewKey = this.perception.join()
-        viewKey = this.verifyMovement(viewKey)
-        if (this.table[viewKey]) {
-            return this.updateCurrentPosition(this.table[viewKey])
-        } else {
-            return this.table["default"]
+        this.updateModel(this.perception)
+        let viewKey = this.perception.join();
+        let accion = this.verifyMovement(this.table[viewKey]);
+        this.updateCurrentPosition(accion);
+
+        //print model
+        let arrText = '';
+        for (var i = 0; i < this.model.length; i++) {
+            for (var j = 0; j < this.model[i].length; j++) {
+                arrText += this.model[i][j] + '    ';
+            }
+            console.log(arrText);
+            arrText = '';
+        }
+        
+        if (accion) {
+            return accion;
         }
     }
 
 }
 
-module.exports = CleanerAgent;
+module.exports = RatonAgent2;
